8ce3c04f81e11e32834642b4442491bb23d2c15a
Carson Slater <carsonslater7@gmail.com>
2025-09-04 14:26:33 -0500

    experimenting with Chatlas

--- MyGraph.py
+++ MyGraph.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from chatlas import ChatOllama
+
+# To activate venv,  `source myvenv/bin/activate`
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
